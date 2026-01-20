f7465d969ce19ef783e9d4ac6d5717f8b556b7bd
Michael Lau <mkl4960@yahoo.com>
2026-01-19 19:04:37 -0500

    login form

---
+++ src/LoginForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Login attempt:', { userId, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>User ID:</label>
+        <input
+          type="text"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
